f5c5a424993c820a39df185166f02dd30dcb537c
Matheus Giarola <matheustpgiarola@gmail.com>
2024-04-28 16:23:29 -0300

    .

--- 2019/p1/distanciaEntreAmigos.py
+++ 2019/p1/distanciaEntreAmigos.py
@@ -1,15 +1,11 @@
 n=int(input())
-hPredios=[]
+hPredios=[int(x) for x in input().split()]
 distanciaMax=0
 aux=0
 
 for i in range(n):
-    x=int(input())
-    hPredios.append(x)
-
-for i in range(hPredios):
-    for j in range(hPredios-i):
-        aux=(hPredios[i]+1)+(hPredios[j]+1)+abs(hPredios.index(hPredios[i])-hPredios.index(hPredios[j]))
+    for j in range(n):
+        aux=(hPredios[i])+(hPredios[j])+abs(hPredios.index(hPredios[i])-hPredios.index(hPredios[j]))
         if aux> distanciaMax:
             distanciaMax=aux
 print(distanciaMax)
