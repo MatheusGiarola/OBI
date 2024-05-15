46f7de7eaeec2763e92ace0bc878e6c98e40ac22
Matheus Giarola <97200978+MatheusGiarola@users.noreply.github.com>
2024-05-15 08:54:17 -0300

    Update distanciaEntreAmigos.py

--- 2019/p1/distanciaEntreAmigos.py
+++ 2019/p1/distanciaEntreAmigos.py
@@ -5,7 +5,7 @@ aux=0
 
 for i in range(n):
     for j in range(n):
-        aux=(hPredios[i])+(hPredios[j])+abs(hPredios.index(hPredios[i])-hPredios.index(hPredios[j]))
+        aux=(hPredios[i])+(hPredios[j])+abs(i-j)
         if aux> distanciaMax:
             distanciaMax=aux
 print(distanciaMax)
