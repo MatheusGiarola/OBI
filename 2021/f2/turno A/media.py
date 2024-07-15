aac3ca7fe3cd9c62b22c5fbe4839e2393b3c04b7
Matheus Giarola <matheustpgiarola@gmail.com>
2024-07-15 13:16:36 -0300

    exercise corrected

--- 2021/f2/turno A/media.py
+++ 2021/f2/turno A/media.py
@@ -1,8 +1,11 @@
 a,b=map(int, input().split())
-x=b/2
-for c in range(b):
-    lista=[a,b,c]
-    lista.sort()
-    if ((a+b+c)/3)==lista[1]:
+for c in range(-b,b):
+    if a<b<c or c<b<a:
+        med=b
+    elif a<c<b or b<c<a:
+        med=c
+    else:
+        med=a
+    if ((a+b+c)/3)==med:
         print(c)
         break
