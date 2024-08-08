f6348bf4ca19ab70f4680cbb3c3a695400aa95d5
Matheus Giarola <matheustpgiarola@gmail.com>
2024-08-07 23:12:25 -0300

    barcos

--- 2023/f2/p2/barcos.py
+++ 2023/f2/p2/barcos.py
@@ -1,46 +1,29 @@
 n,b=map(int, input().split())
-barcos={}
-for a in range(b):
-    i,j,p=map(int, input().split())
-    if i in barcos:
-        barcos[i].append([j,p])
-    else:
-        barcos[i]=[[j,p]]
-    if j in barcos:
-        barcos[j].append([i,p])
-    else:
-        barcos[j]=[[i,p]]
-
+grafo={} #{1: {4: 50, 5: 85, 3: 40}, 4: {1: 50, 2: 80, 5: 75}, 5: {1: 85, 4: 75}, 2: {3: 60, 4: 80}, 3: {2: 60, 1: 40}}
+for i in range(6):
+    x,y,c=map(int, input().split())
+    if x not in grafo:
+        grafo[x]={}
+    grafo[x][y]=c
+    if y not in grafo:
+        grafo[y]={}
+    grafo[y][x]=c
+ilhas=[float('inf') for _ in range(n)] 
 c=int(input())
-for _ in range(c):
-    o,d=map(int, input().split())
-
-    mPassageiros=[]
-    ilhasVisitadas=[]
-    queue=barcos.get(o)
-    while queue:
-        size=len(queue)
-        ilhaAtual=queue[-1]
-
-        if ilhaAtual[0]==d:
-            mPassageiros.append(ilhaAtual[1])
-            queue.pop()
-        else:
-            if ilhaAtual not in ilhasVisitadas:
-                npassageiros=ilhaAtual[1]
-                proximas=barcos.get(ilhaAtual[0])
-                ilhasVisitadas.append(ilhaAtual[0])
-
-                length=len(proximas)
-                queue.pop()
-                for x in range(length):
-                    if x not in ilhasVisitadas:
-                        proximas[x][1]=min(npassageiros,proximas[x][1])
-                        queue.append(proximas[x]) 
-            else:
-                queue.pop()
-    maximo=0
-    for z in mPassageiros:
-        if z>maximo:
-            maximo=z
-    print(maximo)
+for i in range(c):
+    x,y=map(int, input().split())
+    visitados=set()
+    fila=[x]
+    while fila:
+        atual=fila.pop(0)
+        visitados.add(atual)
+        vizinhos=grafo.get(atual)
+        if vizinhos != None:
+            capacidades=[]
+            for vizinho in vizinhos:
+                if vizinho not in visitados:
+                    fila.append(vizinho)
+                    visitados.add(vizinho)
+                
+                ilhas[vizinho-1]=min(ilhas[atual-1],grafo[atual][vizinho])
+    print(ilhas[y-1])
